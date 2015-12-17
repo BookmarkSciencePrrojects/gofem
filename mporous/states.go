a29f7226ad01fea653aa3f9c3eecea69e44a5313
Dorival Pedroso <pedroso@cpmech.com>
2015-12-17 11:15:07 +1000

    liquid-gas-solid implementation initiated: states structure added

--- mporous/states.go
+++ mporous/states.go
@@ -50,6 +50,27 @@ type LsVars struct {
 	DρldusM, DρdusM, DCpldusM               float64
 }
 
+// LgsVars hold data for liquid-gas-solid computations
+type LgsVars struct {
+	A_ρl, A_ρg       float64
+	A_ρ, A_p         float64
+	Cpl, Cpg, Cvs    float64
+	Dpl, Dpg, Dvs    float64
+	Dklrdpl, Dklrdpg float64
+	Dkgrdpl, Dkgrdpg float64
+	Dρdpl, Dρdpg     float64
+	DρdusM           float64
+	Dpdpl, Dpdpg     float64
+	DCpldpl, DCpldpg float64
+	DCpgdpl, DCpgdpg float64
+	DCvsdpl, DCvsdpg float64
+	DDpldpl, DDpldpg float64
+	DDpgdpl, DDpgdpg float64
+	DDvsdpl, DDvsdpg float64
+	DCplduM, DCpgduM float64
+	DDplduM, DDpgduM float64
+}
+
 // CalcLs calculates variables for liquid-solid simulations
 func (o Model) CalcLs(res *LsVars, sta *State, pl, divus float64, derivs bool) (err error) {
 
@@ -102,6 +123,98 @@ func (o Model) CalcLs(res *LsVars, sta *State, pl, divus float64, derivs bool) (
 		res.DρldusM = sl * ρL * ns0
 		res.DρdusM = (sl*ρL - ρS) * ns0       // Eq (A.10) of [1]
 		res.DCpldusM = (sl*Cl - ρL*Ccb) * ns0 // Eq (A.3) of [1]
+	}
+	return
+}
+
+// CalcLgs calculates variables for liquid-gas-solid simulations
+func (o Model) CalcLgs(res *LgsVars, sta *State, pl, pg, divus float64, derivs bool) (err error) {
+
+	// auxiliary
+	ns0 := sta.A_ns0
+	sl := sta.A_sl
+	sg := 1.0 - sl
+	ρL := sta.A_ρL
+	ρG := sta.A_ρG
+	Cl := o.Cl
+	Cg := o.Cg
+	ρS := o.RhoS0
+
+	// n variables
+	ns := (1.0 - divus) * ns0
+	nf := 1.0 - ns
+	nl := nf * sl
+	ng := nf * sg
+
+	// ρ variables
+	ρs := ns * ρS
+	res.A_ρl = nl * ρL
+	res.A_ρg = ng * ρG
+	res.A_ρ = res.A_ρl + res.A_ρg + ρs
+
+	// capillary pressure and pore-fluid pressure
+	pc := pg - pl
+	res.A_p = pl*sl + pg*sg
+
+	// moduli
+	Cc, e := o.Ccb(sta, pc)
+	if e != nil {
+		return e
+	}
+	res.Cpl = nf * (sl*Cl - ρL*Cc)
+	res.Cpg = nf * ρL * Cc
+	res.Cvs = sl * ρL
+	res.Dpl = nf * ρG * Cc
+	res.Dpg = nf * (sg*Cg - ρG*Cc)
+	res.Dvs = sg * ρG
+
+	// derivatives
+	if derivs {
+
+		// Ccd
+		Ccd, e := o.Ccd(sta, pc)
+		if e != nil {
+			return e
+		}
+
+		// conductivity multipliers
+		dklrdsl := o.Cnd.DklrDsl(sl)
+		dkgrdsg := o.Cnd.DkgrDsg(sg)
+		res.Dklrdpl = -dklrdsl * Cc
+		res.Dklrdpg = dklrdsl * Cc
+		res.Dkgrdpl = dkgrdsg * Cc
+		res.Dkgrdpg = -dkgrdsg * Cc
+
+		// mixture density
+		res.Dρdpl = nf * (sl*Cl - ρL*Cc + ρG*Cc)
+		res.Dρdpg = nf * (sg*Cg - ρG*Cc + ρL*Cc)
+		res.DρdusM = (sl*ρL + sg*ρG - ρS) * ns0
+
+		// pressure in pores
+		res.Dpdpl = sl + pc*Cc
+		res.Dpdpg = sg - pc*Cc
+
+		// derivatives of C coefficients
+		res.DCpldpl = nf * (ρL*Ccd - 2.0*Cc*Cl)
+		res.DCpldpg = nf * (Cc*Cl - ρL*Ccd)
+		res.DCpgdpl = nf * (Cl*Cc - ρL*Ccd)
+		res.DCpgdpg = nf * ρL * Ccd
+		res.DCvsdpl = sl*Cl - Cc*ρL
+		res.DCvsdpg = Cc * ρL
+
+		// derivatives of D coefficients
+		res.DDpldpl = -nf * ρG * Ccd
+		res.DDpldpg = nf * (ρG*Ccd + Cg*Cc)
+		res.DDpgdpl = nf * (Cc*Cg + ρG*Ccd)
+		res.DDpgdpg = -nf * (ρG*Ccd + 2.0*Cg*Cc)
+		res.DDvsdpl = Cc * ρG
+		res.DDvsdpg = sg*Cg - Cc*ρG
+
+		// derivatives w.r.t u (multiplier)
+		res.DCplduM = (sl*Cl - ρL*Cc) * ns0
+		res.DCpgduM = ρL * Cc * ns0
+		res.DDplduM = ρG * Cc * ns0
+		res.DDpgduM = (sg*Cg - ρG*Cc) * ns0
 	}
 	return
 }
