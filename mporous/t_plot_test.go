05b0e84ed882b19cad39c6ef8672d5fd301c607d
Dorival Pedroso <pedroso@cpmech.com>
2015-12-19 09:57:04 +1000

    simple plot function of lrm added to mporous

---
+++ mporous/t_plot_test.go
@@ -0,0 +1,60 @@
+// Copyright 2015 Dorival Pedroso and Raul Durand. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mporous
+
+import (
+	"testing"
+
+	"github.com/cpmech/gofem/mconduct"
+	"github.com/cpmech/gofem/mreten"
+	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/plt"
+)
+
+func Test_plot01(tst *testing.T) {
+
+	//verbose()
+	chk.PrintTitle("plot01")
+
+	if !chk.Verbose {
+		return
+	}
+
+	// info
+	simfnk := "derivs01"
+	matname := "mat1"
+	getnew := false
+	example := true
+
+	// conductivity model
+	cnd := mconduct.GetModel(simfnk, matname, "m1", getnew)
+	err := cnd.Init(cnd.GetPrms(example))
+	if err != nil {
+		tst.Errorf("mconduct.Init failed: %v\n", err)
+		return
+	}
+
+	// liquid retention model
+	lrm_name := "ref-m1"
+	//lrm_name := "vg"
+	lrm := mreten.GetModel(simfnk, matname, lrm_name, getnew)
+	err = lrm.Init(lrm.GetPrms(example))
+	if err != nil {
+		tst.Errorf("mreten.Init failed: %v\n", err)
+		return
+	}
+
+	// porous model
+	mdl := GetModel(simfnk, matname, getnew)
+	err = mdl.Init(mdl.GetPrms(example), cnd, lrm)
+	if err != nil {
+		tst.Errorf("mporous.Init failed: %v\n", err)
+		return
+	}
+
+	// plot
+	plt.SetForEps(1.2, 350)
+	PlotSimple(mdl, "gofem", "mporous_plot01.eps", 20, 101, true, true, true)
+}
