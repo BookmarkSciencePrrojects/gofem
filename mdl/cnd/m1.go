6f20b945b5c22e49af81c7e7e5cf27b523faf34f
Dorival Pedroso <pedroso@cpmech.com>
2015-12-21 09:16:52 +1000

    mconduct package moved to subpackage mdl/cnd

---
+++ mdl/cnd/m1.go
@@ -0,0 +1,111 @@
+// Copyright 2015 Dorival Pedroso and Raul Durand. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cnd
+
+import (
+	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/fun"
+)
+
+// M1 implements the liquid-gas conductivity model # 1
+type M1 struct {
+
+	// parameters for liquid
+	λ0l float64
+	λ1l float64
+	αl  float64
+	βl  float64
+
+	// parameters for gas
+	λ0g float64
+	λ1g float64
+	αg  float64
+	βg  float64
+
+	// auxiliary functions
+	klr fun.RefIncRL1
+	kgr fun.RefIncRL1
+}
+
+// add model to factory
+func init() {
+	allocators["m1"] = func() Model { return new(M1) }
+}
+
+// GetPrms gets (an example) of parameters
+func (o M1) GetPrms(example bool) fun.Prms {
+	return fun.Prms{
+		&fun.Prm{N: "lam0l", V: 0.001},
+		&fun.Prm{N: "lam1l", V: 1.2},
+		&fun.Prm{N: "alpl", V: 0.01},
+		&fun.Prm{N: "betl", V: 10},
+		&fun.Prm{N: "lam0g", V: 2.0},
+		&fun.Prm{N: "lam1g", V: 0.001},
+		&fun.Prm{N: "alpg", V: 0.01},
+		&fun.Prm{N: "betg", V: 10},
+	}
+}
+
+// Init initialises this structure
+func (o *M1) Init(prms fun.Prms) (err error) {
+	for _, p := range prms {
+		switch p.N {
+		case "lam0l":
+			o.λ0l = p.V
+		case "lam1l":
+			o.λ1l = p.V
+		case "alpl":
+			o.αl = p.V
+		case "betl":
+			o.βl = p.V
+		case "lam0g":
+			o.λ0g = p.V
+		case "lam1g":
+			o.λ1g = p.V
+		case "alpg":
+			o.αg = p.V
+		case "betg":
+			o.βg = p.V
+		default:
+			return chk.Err("parameter named %q is incorrect\n", p.N)
+		}
+	}
+	err = o.klr.Init(fun.Prms{
+		&fun.Prm{N: "lam0", V: o.λ0l},
+		&fun.Prm{N: "lam1", V: o.λ1l},
+		&fun.Prm{N: "alp", V: o.αl},
+		&fun.Prm{N: "bet", V: o.βl},
+	})
+	if err != nil {
+		return
+	}
+	err = o.kgr.Init(fun.Prms{
+		&fun.Prm{N: "lam0", V: o.λ0g},
+		&fun.Prm{N: "lam1", V: o.λ1g},
+		&fun.Prm{N: "alp", V: o.αg},
+		&fun.Prm{N: "bet", V: o.βg},
+	})
+	return
+}
+
+// Klr returns klr
+func (o M1) Klr(sl float64) float64 {
+	return o.klr.F(sl, nil)
+}
+
+// Kgr returns kgr
+func (o M1) Kgr(sg float64) float64 {
+	return o.kgr.F(sg, nil)
+}
+
+// DklrDsl returns ∂klr/∂sl
+func (o M1) DklrDsl(sl float64) float64 {
+	return o.klr.G(sl, nil)
+}
+
+// DkgrDsl returns ∂kgr/∂sg
+func (o M1) DkgrDsg(sg float64) float64 {
+	return o.kgr.G(sg, nil)
+}
---
+++ mdl/cnd/plotting.go
@@ -0,0 +1,59 @@
+// Copyright 2015 Dorival Pedroso and Raul Durand. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cnd
+
+import (
+	"github.com/cpmech/gosl/io"
+	"github.com/cpmech/gosl/plt"
+	"github.com/cpmech/gosl/utl"
+)
+
+func Plot(o Model, dirout, fname string, np int, gas, withText, deriv bool) {
+	X := utl.LinSpace(0, 1, np)
+	Y := make([]float64, np)
+	var Z []float64
+	if deriv {
+		Z = make([]float64, np)
+	}
+	for i := 0; i < np; i++ {
+		if gas {
+			Y[i] = o.Kgr(X[i])
+		} else {
+			Y[i] = o.Klr(X[i])
+		}
+		if deriv {
+			if gas {
+				Z[i] = o.DkgrDsg(X[i])
+			} else {
+				Z[i] = o.DklrDsl(X[i])
+			}
+		}
+	}
+	if deriv {
+		plt.Subplot(2, 1, 1)
+	}
+	plt.Plot(X, Y, "'b-', clip_on=0")
+	if withText {
+		l := np - 1
+		plt.Text(X[0], Y[0], io.Sf("(%g, %g)", X[0], Y[0]), "ha='left',  color='red', size=8")
+		plt.Text(X[l], Y[l], io.Sf("(%g, %g)", X[l], Y[l]), "ha='right', color='red', size=8")
+	}
+	key := "\\ell"
+	if gas {
+		key = "g"
+	}
+	plt.Gll("$s_{"+key+"}$", "$k_{"+key+"}^r$", "")
+	if deriv {
+		plt.Subplot(2, 1, 2)
+		plt.Plot(X, Z, "'b-', clip_on=0")
+		if withText {
+			l := np - 1
+			plt.Text(X[0], Z[0], io.Sf("(%g, %g)", X[0], Z[0]), "ha='left',  color='red', size=8")
+			plt.Text(X[l], Z[l], io.Sf("(%g, %g)", X[l], Z[l]), "ha='right', color='red', size=8")
+		}
+		plt.Gll("$s_{"+key+"}$", "$\\mathrm{d}{k_{"+key+"}^r}/\\mathrm{d}{s_{"+key+"}}$", "")
+	}
+	plt.SaveD(dirout, fname)
+}
