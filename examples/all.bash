18650f987d3939a9598e8e518d6a3ab835d99693
Dorival Pedroso <pedroso@cpmech.com>
2015-12-28 09:15:16 +1000

    all.bash fixed with new example

--- examples/all.bash
+++ examples/all.bash
@@ -6,13 +6,15 @@ examples="\
 dynamics_sgbook \
 rjoint_ex01_curved \
 rjoint_ex06_pullout \
-seep_simple_flux \
 seep_ex01_freesurf \
 seep_ex02_freesurf \
+seep_simple_flux \
 spo751_pressurised_cylinder \
 spo754_strip_footing_collapse \
 up_3mcolumn_desiccation \
-up_indentation2d_unsat"
+up_indentation2d_unsat \
+upp_3mcolumn_desiccation
+"
 
 for ex in $examples; do
     echo
