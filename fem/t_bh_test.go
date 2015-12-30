044fb5286c84d449d74820aaace17fe8527e0e9b
Dorival Pedroso <pedroso@cpmech.com>
2015-12-30 15:09:59 +1000

    typo fixed

--- fem/t_bh_test.go
+++ fem/t_bh_test.go
@@ -41,7 +41,7 @@ func Test_bh16a(tst *testing.T) {
 		return
 	}
 
-	// initialise solution vectros
+	// initialise solution vectors
 	err = analysis.ZeroStage(0, true)
 	if err != nil {
 		tst.Errorf("ZeroStage failed:\n%v", err)
